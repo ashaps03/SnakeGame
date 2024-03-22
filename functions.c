c6cd6b1c101195146039457e5c22a3a4de7632a0
ashley sanchez <130068110+ashaps03@users.noreply.github.com>
2024-03-22 00:31:16 -0400

    test: Added main.c and function.c files for testing compilation

---
+++ functions.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void myFunction() {
+    printf("Hello from myFunction!\n");
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void myFunction();
+
+int main() {
+    printf("Calling myFunction...\n");
+    myFunction();
+    return 0;
+}
